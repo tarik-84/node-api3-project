2f3c4704691826b236c334da0e2012b5a284fde6
tarik-84 <niceloveman200@yahoo.com>
2020-06-11 14:29:59 -0400

    update

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const server = express();
 
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+// server.get('/', (req, res) => {
+//   res.send(`<h2>Let's write some middleware!</h2>`);
+// });
 
 
 
