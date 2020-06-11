a3fc0fbff00d87ea9d5d514ee6bf8ba76868d795
tarik-84 <niceloveman200@yahoo.com>
2020-06-11 14:23:28 -0400

    Deploying

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const logger = require('./middleware/logger')
 const userRouter = require('./users/userRouter')
 const postRouter= require('./posts/postRouter')
 const server = require('./server')
+const welcomeRouter = require('./welcome/welcome-router')
 
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 server.use(express.json())
 server.use(logger())
+server.use('/', welcomeRouter)
 server.use('/users', userRouter)
 server.use('/posts', postRouter)
 
